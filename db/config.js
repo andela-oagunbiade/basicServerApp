d8f01d20e462363f3370c2d2cbefe91a8c486649
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2019-01-29 12:44:28 +0100

    refactor database connection config

--- db/config.js
+++ db/config.js
@@ -1,28 +1,22 @@
 require('dotenv').config()
 
-module.exports = {
-  test: {
-    dialect: process.env.TEST_DB_DIALECT,
-    host: process.env.TEST_DB_HOST,
-    port: process.env.TEST_DB_PORT,
-    dbName: process.env.TEST_DB_NAME,
-    username: process.env.TEST_DB_USERNAME,
-    password: process.env.TEST_DB_PASSWORD,
-  },
-  development: {
-    dialect: process.env.DEV_DB_DIALECT,
-    host: process.env.DEV_DB_HOST,
-    port: process.env.DEV_DB_PORT,
-    dbName: process.env.DEV_DB_NAME,
-    username: process.env.DEV_DB_USERNAME,
-    password: process.env.DEV_DB_PASSWORD,
-  },
-  'production': {
-    dialect: process.env.PROD_DB_DIALECT,
-    host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
-    dbName: process.env.PROD_DB_NAME,
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
+const envPrefixMap = {
+  test: 'TEST',
+  development: 'DEV',
+  production: 'PROD'
+}
+
+const config = (env) => {
+  const prefix = envPrefixMap[env]
+
+  return {
+    dialect: process.env[`${prefix}_DB_DIALECT`],
+    host: process.env[`${prefix}_DB_HOST`],
+    port: process.env[`${prefix}_DB_PORT`],
+    dbName: process.env[`${prefix}_DB_NAME`],
+    username: process.env[`${prefix}_DB_USERNAME`],
+    password: process.env[`${prefix}_DB_PASSWORD`],
   }
 }
+
+module.exports = config
--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const config = require('./config')
 
 const env = process.env.NODE_ENV
-const { dialect, host, port, dbName, username, password } = config[env]
+const { dialect, host, port, dbName, username, password } = config(env)
 
 const sequelize = new Sequelize(`${dialect}://${username}:${password}@${host}:${port}/${dbName}`)
 
