7abe5982671b00897fff5836f24a48d510999d78
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2019-01-29 11:58:43 +0100

    configure sequelize connection to a potgres db

---
+++ db/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const Sequelize = require('sequelize')
+const config = require('./config')
+
+const env = process.env.NODE_ENV
+const { dialect, host, port, dbName, username, password } = config[env]
+
+const sequelize = new Sequelize(`${dialect}://${username}:${password}@${host}:${port}/${dbName}`)
+
+module.exports = sequelize
